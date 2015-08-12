e84bcb1239cdbbc652215457360ecb232e7c384b
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-08-12 22:20:19 +0200

    end-of-day commit

---
+++ LinkedListExample/LinkedListExample.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include <LinkedList.h>
+
+
+void setup() {
+  LinkedList<WiFi> *wifis = new LinkedList<WiFi>();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
